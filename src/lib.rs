8299be39d0cf359e6939b17605128f5f0186214a
Nick Ufer <nick@ufer.dev>
2021-10-04 21:04:21 +0200

    feat: adds initialization of addon

--- src/lib.rs
+++ src/lib.rs
@@ -76,4 +76,63 @@ struct AuthRootContext {
 
 impl Context for AuthRootContext {}
 
-impl RootContext for AuthRootContext {}
+impl RootContext for AuthRootContext {
+    fn on_vm_start(&mut self, _vm_configuration_size: usize) -> bool {
+        true
+    }
+
+    fn on_configure(&mut self, _plugin_configuration_size: usize) -> bool {
+        if let config_bytes = self.get_configuration().unwrap() {
+            let cfg: RawConfig = serde_json::from_slice(config_bytes.as_slice()).unwrap();
+
+            {
+                let jwt_algorithm_name: Algorithm;
+                match Algorithm::from_str(cfg.jwt.algorithm_name.as_str()) {
+                    Ok(algorithm) => {
+                        jwt_algorithm_name = algorithm;
+                    }
+                    Err(error) => {
+                        log(
+                            LogLevel::Error,
+                            format!("JWT encoding key error: {:?}", error).as_str(),
+                        );
+                        return false;
+                    }
+                }
+
+                let jwt_encoding_key: EncodingKey;
+                match EncodingKey::from_rsa_pem(cfg.jwt.key.as_bytes()) {
+                    Ok(encoding_key) => {
+                        jwt_encoding_key = encoding_key;
+                    }
+                    Err(error) => {
+                        log(
+                            LogLevel::Error,
+                            format!("JWT encoding key error: {:?}", error).as_str(),
+                        );
+                        return false;
+                    }
+                }
+                self.jwt_producer =
+                    JwtProducer::from(Header::new(jwt_algorithm_name), jwt_encoding_key)
+            }
+            {
+                // TODO validation
+                self.introspect_config = IntrospectConfig {
+                    upstream: cfg.introspect_endpoint.upstream,
+                    path: cfg.introspect_endpoint.path,
+                    authority: cfg.introspect_endpoint.authority,
+                }
+            }
+        }
+        true
+    }
+
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
+        let http_context = AuthHttpContext {
+            jwt_producer: self.jwt_producer.clone(),
+            introspect_config: self.introspect_config.clone(),
+        };
+        return Some(Box::from(http_context));
+    }
+}
