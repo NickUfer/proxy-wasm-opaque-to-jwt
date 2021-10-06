4af2c5129c9d75168dc066d933beeb665096db34
Nick Ufer <nick@ufer.dev>
2021-10-06 23:03:10 +0200

    feat: first working version

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,30 +1,220 @@
 pub mod jwt_producer {
-    use jsonwebtoken::{Algorithm, EncodingKey, Header};
+    use dyn_clone::DynClone;
+    use jwt_simple::algorithms::*;
+    use jwt_simple::claims::Audiences::AsSet;
+    use jwt_simple::prelude::*;
+    use serde::{Deserialize, Serialize};
     use serde_json::Value;
+    use std::str::FromStr;
 
     #[derive(Clone)]
     pub struct JwtProducer {
-        header: Header,
-        encoding_key: EncodingKey,
+        key_pair: Option<Box<dyn Encoder>>,
     }
 
     impl JwtProducer {
         pub fn noop() -> JwtProducer {
+            JwtProducer {
+                key_pair: Option::Some(Box::new(NoopEncoder {})),
+            }
+        }
+
+        pub fn from(algorithm: Algorithm, key: String) -> JwtProducer {
+            let key_pair: Box<dyn Encoder> = match algorithm {
+                Algorithm::RS256 => Box::new(RS256KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::HS256 => Box::new(HS256Key::from_bytes(key.as_bytes())),
+                Algorithm::HS384 => Box::new(HS384Key::from_bytes(key.as_bytes())),
+                Algorithm::HS512 => Box::new(HS512Key::from_bytes(key.as_bytes())),
+                Algorithm::RS384 => Box::new(RS384KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::RS512 => Box::new(RS512KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::PS256 => Box::new(PS256KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::PS384 => Box::new(PS384KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::PS512 => Box::new(PS512KeyPair::from_pem(key.as_str()).unwrap()),
+                Algorithm::EdDSA => Box::new(Ed25519KeyPair::from_pem(key.as_str()).unwrap()),
+                _ => Box::new(NoopEncoder {}),
+            };
             return JwtProducer {
-                header: Header::default(),
-                encoding_key: EncodingKey::from_secret("".as_bytes()),
+                key_pair: Option::Some(key_pair),
             };
         }
 
-        pub fn from(header: Header, encoding_key: EncodingKey) -> JwtProducer {
-            return JwtProducer {
-                header,
-                encoding_key,
-            };
+        pub fn encode_jwt(&self, claims: Value) -> String {
+            self.key_pair.as_ref().unwrap().encode_jwt(claims)
         }
+    }
 
-        pub fn encode(&self, claims: &Value) -> String {
-            jsonwebtoken::encode(&self.header, claims, &self.encoding_key).unwrap()
+    pub trait Encoder: DynClone {
+        fn encode_jwt(&self, claims: Value) -> String; // TODO refactor to return Result
+    }
+
+    dyn_clone::clone_trait_object!(Encoder);
+
+    #[derive(Clone)]
+    struct NoopEncoder {}
+
+    impl Encoder for NoopEncoder {
+        fn encode_jwt(&self, _: Value) -> String {
+            String::from("NoopEncoder") // FIXME error
+        }
+    }
+
+    impl Encoder for RS512KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for RS384KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for RS256KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for PS512KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for PS384KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for PS256KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for HS256Key {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.authenticate(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for HS384Key {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.authenticate(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for HS512Key {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.authenticate(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    impl Encoder for Ed25519KeyPair {
+        fn encode_jwt(&self, claims: Value) -> String {
+            return self.sign(convert_value_to_claims(claims)).unwrap();
+        }
+    }
+
+    fn convert_value_to_claims(value: Value) -> JWTClaims<NoCustomClaims> {
+        let mut audiences = HashSet::new();
+        for audience in value.get("aud").unwrap().as_array().unwrap() {
+            audiences.insert(String::from(audience.as_str().unwrap()));
+        }
+        return JWTClaims {
+            issued_at: Option::Some(UnixTimeStamp::from_secs(
+                value.get("iat").unwrap().as_u64().unwrap(),
+            )),
+            expires_at: Option::Some(UnixTimeStamp::from_secs(
+                value.get("exp").unwrap().as_u64().unwrap(),
+            )),
+            invalid_before: Option::Some(UnixTimeStamp::from_secs(
+                value.get("nbf").unwrap().as_u64().unwrap(),
+            )),
+            audiences: Option::Some(AsSet(audiences)),
+            issuer: Option::Some(String::from(value.get("iss").unwrap().as_str().unwrap())),
+            jwt_id: None,
+            subject: Option::Some(String::from(value.get("sub").unwrap().as_str().unwrap())),
+            nonce: None,
+            custom: NoCustomClaims {},
+        };
+    }
+
+    // The MIT License (MIT)
+    //
+    // Copyright (c) 2015 Vincent Prouillet
+    //
+    // Permission is hereby granted, free of charge, to any person obtaining a copy
+    // of this software and associated documentation files (the "Software"), to deal
+    // in the Software without restriction, including without limitation the rights
+    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    // copies of the Software, and to permit persons to whom the Software is
+    // furnished to do so, subject to the following conditions:
+    //
+    // The above copyright notice and this permission notice shall be included in all
+    // copies or substantial portions of the Software.
+    //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    // SOFTWARE.
+    /// The algorithms supported for signing/verifying JWTs
+    #[derive(Debug, PartialEq, Hash, Copy, Clone, Serialize, Deserialize)]
+    pub enum Algorithm {
+        /// HMAC using SHA-256
+        HS256,
+        /// HMAC using SHA-384
+        HS384,
+        /// HMAC using SHA-512
+        HS512,
+
+        /// ECDSA using SHA-256
+        ES256,
+        /// ECDSA using SHA-384
+        ES384,
+
+        /// RSASSA-PKCS1-v1_5 using SHA-256
+        RS256,
+        /// RSASSA-PKCS1-v1_5 using SHA-384
+        RS384,
+        /// RSASSA-PKCS1-v1_5 using SHA-512
+        RS512,
+
+        /// RSASSA-PSS using SHA-256
+        PS256,
+        /// RSASSA-PSS using SHA-384
+        PS384,
+        /// RSASSA-PSS using SHA-512
+        PS512,
+
+        /// Edwards-curve Digital Signature Algorithm (EdDSA)
+        EdDSA,
+    }
+
+    impl FromStr for Algorithm {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Algorithm, String> {
+            match s.to_uppercase().as_str() {
+                "HS256" => Ok(Algorithm::HS256),
+                "HS384" => Ok(Algorithm::HS384),
+                "HS512" => Ok(Algorithm::HS512),
+                "ES256" => Ok(Algorithm::ES256),
+                "ES384" => Ok(Algorithm::ES384),
+                "RS256" => Ok(Algorithm::RS256),
+                "RS384" => Ok(Algorithm::RS384),
+                "PS256" => Ok(Algorithm::PS256),
+                "PS384" => Ok(Algorithm::PS384),
+                "PS512" => Ok(Algorithm::PS512),
+                "RS512" => Ok(Algorithm::RS512),
+                "EDDSA" => Ok(Algorithm::EdDSA),
+                _ => Err(String::from("Algorithm invalid")),
+            }
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-use crate::jwt::jwt_producer::JwtProducer;
-use jsonwebtoken::{Algorithm, EncodingKey, Header};
+use crate::jwt::jwt_producer::{Algorithm, JwtProducer};
 use proxy_wasm::hostcalls::log;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-use serde::Deserialize;
+use serde_derive::Deserialize;
 use serde_json::Value;
-use std::collections::HashMap;
-use std::fmt::Error;
-use std::ops::Deref;
 use std::str::FromStr;
 use std::time::Duration;
 
@@ -15,9 +11,10 @@ mod jwt;
 
 #[no_mangle]
 pub fn _start() {
-    proxy_wasm::set_log_level(LogLevel::Trace);
-    proxy_wasm::set_root_context(|context_id| -> Box<dyn RootContext> {
+    proxy_wasm::set_log_level(LogLevel::Trace); // FIXME
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
         Box::new(AuthRootContext {
+            jwt_header_name: String::new(),
             jwt_producer: JwtProducer::noop(),
             introspect_config: IntrospectConfig {
                 upstream: String::new(),
@@ -39,7 +36,7 @@ struct RawConfig {
 
 #[derive(Deserialize)]
 struct RawConfigJwt {
-    algorithm_name: String,
+    algorithm: String,
     key: String,
     output_header_name: String,
 }
@@ -59,6 +56,7 @@ struct IntrospectConfig {
 }
 
 struct AuthHttpContext {
+    jwt_header_name: String,
     jwt_producer: JwtProducer,
     introspect_config: IntrospectConfig,
 }
@@ -74,7 +72,7 @@ impl AuthHttpContext {
                 (":method", "POST"),
                 (":path", self.introspect_config.path.as_str()),
                 (":authority", self.introspect_config.authority.as_str()),
-                (":content-type", "application/x-www-form-urlencoded"),
+                ("content-type", "application/x-www-form-urlencoded"),
             ],
             Option::Some(encoded_body.into_bytes().as_slice()),
             vec![],
@@ -83,11 +81,74 @@ impl AuthHttpContext {
     }
 }
 
-impl Context for AuthHttpContext {}
+impl Context for AuthHttpContext {
+    fn on_http_call_response(
+        &mut self,
+        _token_id: u32,
+        _num_headers: usize,
+        body_size: usize,
+        _num_trailers: usize,
+    ) {
+        match self.get_http_call_response_body(0, body_size) {
+            None => {
+                self.resume_http_request();
+            }
+            Some(response_bytes) => {
+                let value: Value = serde_json::from_slice(response_bytes.as_slice()).unwrap();
 
-impl HttpContext for AuthHttpContext {}
+                if !value.get("active").unwrap().as_bool().unwrap() {
+                    self.resume_http_request();
+                    return;
+                }
+
+                self.add_http_request_header(
+                    self.jwt_header_name.as_str(),
+                    self.jwt_producer.encode_jwt(value).as_str(),
+                );
+                self.resume_http_request();
+            }
+        }
+    }
+}
+
+impl HttpContext for AuthHttpContext {
+    fn on_http_request_headers(&mut self, _: usize) -> Action {
+        let token: String;
+        match self.get_http_request_header("authorization") {
+            Some(auth_header) => {
+                if !auth_header.starts_with("Bearer ") {
+                    log(
+                        LogLevel::Debug,
+                        format!(
+                            "Authorization header not a Bearer token, skipping. Header: {}",
+                            auth_header
+                        )
+                        .as_str(),
+                    );
+                    return Action::Continue;
+                }
+                // Removes Bearer from header to get actual token
+                // Expects there are no more spaces
+                token = String::from(&auth_header[7..auth_header.len()]);
+            }
+            _ => return Action::Continue, // No header, skip auth any further processing
+        }
+
+        return match self.introspect_token(token.as_str()) {
+            Ok(_) => Action::Pause,
+            Err(error) => {
+                log(
+                    LogLevel::Error,
+                    format!("Error while calling introspect endpoint: {:?}", error).as_str(),
+                );
+                Action::Continue
+            }
+        };
+    }
+}
 
 struct AuthRootContext {
+    jwt_header_name: String,
     jwt_producer: JwtProducer,
     introspect_config: IntrospectConfig,
 }
@@ -100,47 +161,46 @@ impl RootContext for AuthRootContext {
     }
 
     fn on_configure(&mut self, _plugin_configuration_size: usize) -> bool {
-        if let config_bytes = self.get_configuration().unwrap() {
-            let cfg: RawConfig = serde_json::from_slice(config_bytes.as_slice()).unwrap();
-
-            {
-                let jwt_algorithm_name: Algorithm;
-                match Algorithm::from_str(cfg.jwt.algorithm_name.as_str()) {
-                    Ok(algorithm) => {
-                        jwt_algorithm_name = algorithm;
-                    }
-                    Err(error) => {
-                        log(
-                            LogLevel::Error,
-                            format!("JWT encoding key error: {:?}", error).as_str(),
-                        );
-                        return false;
-                    }
+        match self.get_configuration() {
+            Some(config_bytes) => {
+                let cfg: RawConfig = serde_json::from_slice(config_bytes.as_slice()).unwrap();
+                if !cfg.jwt.output_header_name.is_empty() {
+                    self.jwt_header_name = cfg.jwt.output_header_name;
+                } else {
+                    self.jwt_header_name = String::from("x-jwt-user");
                 }
 
-                let jwt_encoding_key: EncodingKey;
-                match EncodingKey::from_rsa_pem(cfg.jwt.key.as_bytes()) {
-                    Ok(encoding_key) => {
-                        jwt_encoding_key = encoding_key;
+                {
+                    let jwt_algorithm: Algorithm;
+                    match Algorithm::from_str(cfg.jwt.algorithm.as_str()) {
+                        Ok(algorithm) => {
+                            jwt_algorithm = algorithm;
+                        }
+                        Err(error) => {
+                            log(
+                                LogLevel::Error,
+                                format!("JWT encoding key error: {:?}", error).as_str(),
+                            );
+                            return false;
+                        }
                     }
-                    Err(error) => {
-                        log(
-                            LogLevel::Error,
-                            format!("JWT encoding key error: {:?}", error).as_str(),
-                        );
-                        return false;
+
+                    self.jwt_producer = JwtProducer::from(jwt_algorithm, cfg.jwt.key);
+                }
+                {
+                    // TODO validation
+                    self.introspect_config = IntrospectConfig {
+                        upstream: cfg.introspect_endpoint.upstream,
+                        path: cfg.introspect_endpoint.path,
+                        authority: cfg.introspect_endpoint.authority,
                     }
                 }
-                self.jwt_producer =
-                    JwtProducer::from(Header::new(jwt_algorithm_name), jwt_encoding_key)
             }
-            {
-                // TODO validation
-                self.introspect_config = IntrospectConfig {
-                    upstream: cfg.introspect_endpoint.upstream,
-                    path: cfg.introspect_endpoint.path,
-                    authority: cfg.introspect_endpoint.authority,
-                }
+            None => {
+                log(
+                    LogLevel::Error,
+                    "Failed to read non existent configuration.",
+                );
             }
         }
         true
@@ -148,9 +208,14 @@ impl RootContext for AuthRootContext {
 
     fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
         let http_context = AuthHttpContext {
+            jwt_header_name: self.jwt_header_name.clone(),
             jwt_producer: self.jwt_producer.clone(),
             introspect_config: self.introspect_config.clone(),
         };
         return Some(Box::from(http_context));
+    }
+
+    fn get_type(&self) -> Option<ContextType> {
+        Option::Some(ContextType::HttpContext)
     }
 }
